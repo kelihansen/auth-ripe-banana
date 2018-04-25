2b73af56bc945049341d510d99dddeb644e92344
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-24 20:04:09 -0700

    add date validation and generate ids for 'foreign keys'

--- test/unit/Film.test.js
+++ test/unit/Film.test.js
@@ -1,62 +1,58 @@
 const { assert } = require('chai');
+const { Types } = require('mongoose');
 const Film = require('../../lib/models/Film');
-const Actor = require('../../lib/models/Actor');
-const Studio = require('../../lib/models/Studio');
 const { getErrors } = require('./helpers');
 
 describe('Film model', () => {
+    it('is a good, valid model', () => {
 
-    const sarah = {
-        name: 'Sarah Vowell'
-    };
-
-    const pixar = {
-        name: 'Pixar'
-    };
-
-    const actor = new Actor(sarah);
-    const studio = new Studio(pixar);
-
-    it('valid good model', () => {
-
-        const data = {
+        const info = {
             title: 'The Incredibles',
-            studio: studio._id,
-            released: 2004,
-            cast: []
-        };
-
-        const film = new Film(data);
-        data._id = film._id;
-        assert.deepEqual(film.toJSON(), data);
-
-        assert.isUndefined(film.validateSync());
-    });
-
-    it('required fields', () => {
-        const film = new Film({});
-        const errors = getErrors(film.validateSync(), 3);
-        assert.equal(errors.title.kind, 'required');
-        assert.equal(errors.studio.kind, 'required');
-        assert.equal(errors.released.kind, 'required');
-    });
-
-    it('checks actor id', () => {
-
-        const data = {
-            title: 'The Incredibles',
-            studio: studio._id,
+            studio: Types.ObjectId(),
             released: 2004,
             cast: [{
                 part: 'Violet Parr',
-                actor: actor._id
+                actor: Types.ObjectId()
             }]
         };
 
-        const film = new Film(data);
-
-        assert.deepEqual(actor._id, film.cast[0].actor);
+        const film = new Film(info);
+        info._id = film._id;
+        info.cast[0]._id = film.cast[0]._id;
+        assert.deepEqual(film.toJSON(), info);
         assert.isUndefined(film.validateSync());
+    });
+
+    it('has required fields', () => {
+        const film = new Film({ cast: [{}] });
+        const errors = getErrors(film.validateSync(), 4);
+        assert.equal(errors.title.kind, 'required');
+        assert.equal(errors.studio.kind, 'required');
+        assert.equal(errors.released.kind, 'required');
+        assert.equal(errors['cast.0.actor'].kind, 'required');
+    });
+
+    it('takes a 4-digit number as a release date', () => {
+
+        const tooLowInfo = {
+            title: 'test',
+            studio: Types.ObjectId(),
+            released: 18,
+        };
+        
+        const tooHighInfo = {
+            title: 'test',
+            studio: Types.ObjectId(),
+            released: 42418,
+        };
+
+        const tooLowFilm = new Film(tooLowInfo);
+        const lowError = getErrors(tooLowFilm.validateSync(), 1);
+        assert.equal(lowError.released.kind, 'min');
+        
+        const tooHighFilm = new Film(tooHighInfo);
+        const highError = getErrors(tooHighFilm.validateSync(), 1);
+        assert.equal(highError.released.kind, 'max');
     });
 
 });
