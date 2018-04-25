af2854ccb89d4525d028473a648a24f36a97b422
Keli Hansen <keli.p.hansen@gmail.com>
2018-04-24 20:12:34 -0700

    remove trailing commas

--- test/unit/Review.test.js
+++ test/unit/Review.test.js
@@ -4,6 +4,7 @@ const Review = require('../../lib/models/Review');
 const { getErrors } = require('./helpers');
 
 describe('Review model', () => {
+    
     it('is a good, valid model', () => {
         const info = {
             rating: 5,
@@ -35,7 +36,7 @@ describe('Review model', () => {
             rating: 0,
             reviewer: Types.ObjectId(),            
             review: 'test',
-            film: Types.ObjectId(),            
+            film: Types.ObjectId()            
         });
         const errors = getErrors(review.validateSync(), 1);
         assert.strictEqual(errors.rating.kind, 'min');
@@ -46,7 +47,7 @@ describe('Review model', () => {
             rating: 10,
             reviewer: Types.ObjectId(),            
             review: ' Think about it a little and, my god, it\'s about you. Whoever you are. Here is how life is supposed to work. We come out of ourselves and unfold into the world. We try to realize our desires. We fold back into ourselves, and then we die.',
-            film: Types.ObjectId(),            
+            film: Types.ObjectId()          
         });
         const errors = getErrors(review.validateSync(), 2);
         assert.strictEqual(errors.rating.kind, 'max');
